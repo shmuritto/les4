705c5c7f76eb915ccd9db222ea9044bbe240f712
Artyom Kapustin <shmuritto@gmail.com>
2023-03-29 21:26:06 +0600

    fix 1

--- src/Calc.java
+++ src/Calc.java
@@ -1,7 +1,14 @@
 public class Calc {
     public static void main(String[] args) {
-        Square box = new Square(12, 5,5,4,5,6);
+        Square box = new Square(5, 5);
         System.out.println(box.getArea());
         System.out.println(box.getPerimeter());
+        System.out.println(box.x);
+        System.out.println(box.y);
+
+        Rectangle rec = new Rectangle(3, 4, 5);
+        System.out.println(rec.getPerimeter());
+        System.out.println(rec.getArea());
+        System.out.println(rec.h);
     }
 }
